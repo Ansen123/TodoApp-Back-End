36bb63eacdb12e39e804ce2d9ada67e822a14c43
Ansen123 <ansenfp@gmail.com>
2022-10-26 19:08:02 +0530

    first commit

---
+++ src/main/java/com/nestdigital/todoApp/Dao/PostDao.java
@@ -0,0 +1,15 @@
+package com.nestdigital.todoApp.Dao;
+
+
+import com.nestdigital.todoApp.Model.PostModel;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Map;
+
+public interface PostDao extends CrudRepository<PostModel,Integer> {
+
+    @Query(value = "SELECT * FROM `posts` WHERE `user_id`= :user_id",nativeQuery = true)
+    List<Map<String,String>> ViewAllSingle(Integer user_id);
+}
